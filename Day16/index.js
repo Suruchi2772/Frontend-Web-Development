856e17c710638d01a87663c263a3f5cb8d6c93d3
Suruchi Sharma <suru2772chisharma@gmail.com>
2025-01-18 22:38:25 +0530

    Day16

---
+++ Day16/index.js
@@ -0,0 +1,63 @@
+// Javascript is a scripting language.
+// Javascript is dynamically typed language.
+// Javascript is interpreted not compiled.
+
+// Keywords : keywords are reserved word that have a specific meaning to javascript.for eg- let, const, var, for, if etc.
+
+// Ways to create variables:let var and const
+// let name = "Vishal"; //let is a new way of defining variables in es6
+// // let name = "Suruchi"; redeclaration is not allowed
+// name = "Suruchi"; //re-assignment is allowed
+// console.log(name);
+
+// var firstName = "Vishal"; //var is an old way of defining variables in es5
+// var firstName = "Suruchi"; //redeclaration is allowed
+// firstName = "Om"; //re-assignment is allowed
+// console.log(firstName);
+
+// const creditCardNumber = 123456789;
+// // const creditCardNumber = 123; redeclaration is not allowed
+// // creditCardNumber = 23456; reassignment is not allowed
+// console.log(creditCardNumber);
+
+
+// Datatypes: Datatypes are the type of data that a variable can hold
+// Number,bigInt , String, null, Boolean, undefined, symbol, arrays, objects,
+
+// let empId = 2345;
+// console.log(typeof empId); //datatype is number
+
+// let empSalary = 23000.50;
+// console.log(typeof empSalary);
+
+// let empAccNumber = BigInt(1234567890022);
+// console.log(empAccNumber); //datatype is bigint
+
+// let empName = "Vishal";
+// console.log(typeof empName); //datatype is string
+
+// let isPresent = true;
+// console.log(typeof isPresent); //datatype is boolean
+
+// let isSelected;
+// console.log(typeof isSelected); //datatype is undefined
+
+// let istrue = null;
+// console.log(typeof istrue); //datatype is object
+
+// let list = ["apple", "banana", "peach", "cucumber"];
+// console.log(typeof list); //datatype is object
+// console.log(list);
+
+// let student = {
+//     name: "Suruchi",
+//     age: 22,
+//     sex: "female"
+// }
+// console.log(typeof student); //datatype is object
+// console.log(student);
+
+
+// Reference and primitive datatype
+// Datatype except {},[],() are called primitive datatype
+
